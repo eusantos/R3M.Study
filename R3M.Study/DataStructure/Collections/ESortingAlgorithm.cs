a3e60d5d75527a2388a3efa9e6e2343883f581fe
Renato Medeiros <renatommm@juntoseguros.com>
2021-01-11 15:56:36 -0300

    Generates IList interface

---
+++ R3M.Study/DataStructure/Collections/ESortingAlgorithm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructure.Collections
+{
+    public enum ESortingAlgorithm
+    {
+        BubbleSort,
+        HeapSort,
+        MergeSort,        
+        QuickSort,
+        TreeSort
+    }
+}
--- R3M.Study/DataStructure/Collections/IList.cs
+++ R3M.Study/DataStructure/Collections/IList.cs
@@ -40,5 +40,41 @@ namespace DataStructure.Collections
         /// </summary>
         /// <param name="elem"></param>
         void AddStrict(T elem, int index);
+        /// <summary>
+        /// Removes the element at the first position of the list
+        /// </summary>
+        /// <param name="elem"></param>
+        void RemoveFirst();
+        /// <summary>
+        /// Removes the element at the last position of the list
+        /// </summary>
+        /// <param name="elem"></param>
+        void RemoveLast();
+        /// <summary>
+        /// Removes the first element that satisfies the condition.
+        /// If no matching elements, nothing happens.
+        /// </summary>
+        /// <param name="elem"></param>
+        void Remove(Func<bool, bool> func);
+        /// <summary>
+        /// Removes all elements that satisfy the condition
+        /// If no matching elements, nothing happens.
+        /// </summary>
+        /// <param name="elem"></param>
+        void RemoveAll(Func<bool, bool> func);
+        /// <summary>
+        /// Randomly switches the position of the list elements.
+        /// </summary>
+        void Shuffle();
+        /// <summary>
+        /// Sorts the list accordingly to the comparison function using the default algorithm
+        /// </summary>
+        /// <param name="func"></param>
+        void Sort(Func<T, T, int> func);
+        /// <summary>
+        /// Sorts the list accordingly to the comparison function using the sortingAlgorithm algorithm
+        /// </summary>
+        /// <param name="sortingAlgorithm"></param>
+        void Sort(Func<T, T, int> func, ESortingAlgorithm sortingAlgorithm);
     }
 }
