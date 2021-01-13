0a8d1c4792530c6cb59ac08261f712e24a377387
Renato Medeiros <renatommm@juntoseguros.com>
2021-01-12 22:06:56 -0300

    Implements bubblesort algorithm

---
+++ R3M.Study/DataStructure/Collections/Sorting/BubbleSort.cs
@@ -0,0 +1,32 @@
+﻿using DataStructure.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructure.Collections.Sorting
+{
+    public class BubbleSort<T> : ISortable<T>
+    {
+        public void Sort(IList<T> target, Func<T, T, ETwoObjectsComparison> func)
+        {
+            if (target == null)
+                throw new ArgumentNullException("target");
+            if (target.Empty())
+                return;
+
+            bool switched;
+            do
+            {
+                switched = false;
+                for (int i = 0; i < target.Count() - 1; i++)
+                {
+                    if (func.Invoke(target.At(i), target.At(i + 1)) == ETwoObjectsComparison.FirstElementGreater)
+                    {
+                        target.Swap(i, i + 1);
+                        switched = true;
+                    }
+                }
+            } while (switched);
+        }
+    }
+}
---
+++ R3M.Study/DataStructure/Collections/Sorting/ISortable.cs
@@ -0,0 +1,12 @@
+﻿using DataStructure.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructure.Collections.Sorting
+{
+    public interface ISortable<T>
+    {
+        void Sort(IList<T> target, Func<T, T, ETwoObjectsComparison> func);
+    }
+}
